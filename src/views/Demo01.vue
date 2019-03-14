830316028c2fd3bd88733ff8ac7fda3c4a8b0eed
zhujuntian <2424647830@qq.com>
2019-03-14 10:25:21 +0800

    ts两种渲染模式

---
+++ src/views/Demo01.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <button @click="onClick">Click!</button>
+    <div>{{count}}</div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component({})
+export default class Demo01 extends Vue {
+    //可直接申明实例的属性
+    message: string = 'Hello'
+    count: number = 0
+
+    mounted(){
+        console.log("mounted");
+    }
+
+    //可直接申明方法
+    onClick(): void {
+        // window.alert(this.message);
+        this.count++;
+    }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Demo02.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <button @click="onClick">Click!</button>
+    <div>{{count}}</div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    mounted() {
+        console.log("mounted");
+    },
+    methods: {
+        onClick(): void {
+            // window.alert(this.message);
+            this.count++;
+        }
+    }
+})
+</script>
+
+<style scoped lang="scss">
+
+</style>
